6c6551dcbae7f2754804bf55f95a4d6370644894
Toan <phanthanhtoan248@gmail.com>
2023-11-09 14:24:39 +0700

    Add project files.

---
+++ WebApplication5/JavaScript.js
@@ -0,0 +1,19 @@
+﻿
+document.addEventListener("DOMContentLoaded", function () {
+    var p1 = document.getElementById("p1");
+    var adressFormArrive = document.getElementById("adress-form-arrive");
+    var adressForm = document.getElementById("adress-form");
+
+    p1.addEventListener("click", function () {
+        adressForm.style.display = "flex";
+    });
+
+    adressFormArrive.addEventListener("click", function (event) {
+        event.preventDefault();
+        adressForm.style.display = "flex";
+    });
+});
+const adressClose = document.querySelector('.adress-close');
+adressClose.addEventListener("click", function () {
+    document.querySelector('.adress-form').style.display = "none";
+});
